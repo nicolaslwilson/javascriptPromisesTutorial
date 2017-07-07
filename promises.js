f6138bd3417e82e666fcd298b709cb7c6c7cdec1
Nic Wilson <nicwilson@gmail.com>
2017-07-07 14:22:53 -0500

    Using .reduce() to execute requests in sequence

--- promises.js
+++ promises.js
@@ -50,23 +50,81 @@ const request = require('request');
 
 //Using Promise.all()
 
+// const queryParameters = ['ahoy', 'hello', 'hallo'];
+
+// const fetchPromises = queryParameters.map(queryParam => {
+//   return fetch(`http://httpbin.org/get?${queryParam}`)
+//     .then(response => {
+//       // parse response body as JSON
+//       return response.json()
+//     })
+//     .then(response => {
+//       // extract the URL property from the response object
+//       let url = response.url;
+//       console.log('Response from: %s', url);
+//       return url;
+//     });
+// });
+
+// Promise.all(fetchPromises).then(allUrls => {
+//   console.log('The return values of all requests are passed as an array:');
+//   console.log(allUrls);
+// }).catch(error => {
+//   console.error('A call failed:');
+//   console.error(error.message);
+// });
+
+// Executing Promises in Series
+
+// const queryParameters = ['ahoy', 'hello', 'hallo'];
+
+// let mostRecentPromise = Promise.resolve([]); // start with an immediately resolving promise and an empty list
+// queryParameters.forEach(queryParam => {
+//   // chain the promise to the previous one
+//   mostRecentPromise = mostRecentPromise.then(requestedUrlsSoFar => {
+//     return fetch(`http://httpbin.org/get?${queryParam}`)
+//       .then(response => {
+//         // parse response body as JSON
+//         return response.json()
+//       })
+//       .then(response => {
+//         // extract the URL property from the response object
+//         let url = response.url;
+//         console.log('Response from: %s', url);
+//         requestedUrlsSoFar.push(url);
+//         return requestedUrlsSoFar;
+//       });
+//   });
+// });
+
+// mostRecentPromise.then(allUrls => {
+//   console.log('The return values of all requests are passed as an array:');
+//   console.log(allUrls);
+// }).catch(error => {
+//   console.error('A call failed:');
+//   console.error(error.message);
+// });
+
 const queryParameters = ['ahoy', 'hello', 'hallo'];
 
-const fetchPromises = queryParameters.map(queryParam => {
-  return fetch(`http://httpbin.org/get?${queryParam}`)
-    .then(response => {
-      // parse response body as JSON
-      return response.json()
-    })
-    .then(response => {
-      // extract the URL property from the response object
-      let url = response.url;
-      console.log('Response from: %s', url);
-      return url;
-    });
-});
+let mostRecentPromise = queryParameters.reduce((previousPromise, queryParam) => {
+  return previousPromise.then(requestedUrlsSoFar => {
+    return fetch(`http://httpbin.org/get?${queryParam}`)
+      .then(response => {
+        // parse response body as JSON
+        return response.json()
+      })
+      .then(response => {
+        // extract the URL property from the response object
+        let url = response.url;
+        console.log('Response from: %s', url);
+        requestedUrlsSoFar.push(url);
+        return requestedUrlsSoFar;
+      });
+  });
+}, Promise.resolve([]));
 
-Promise.all(fetchPromises).then(allUrls => {
+mostRecentPromise.then(allUrls => {
   console.log('The return values of all requests are passed as an array:');
   console.log(allUrls);
 }).catch(error => {
