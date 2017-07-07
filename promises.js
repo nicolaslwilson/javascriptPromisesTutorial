ae99287533961fabe709583a6ec83ea0416edf13
Nic Wilson <nicwilson@gmail.com>
2017-07-07 13:07:47 -0500

    Refactored to chain multiple .then() calls together

--- promises.js
+++ promises.js
@@ -3,6 +3,8 @@
 const fetch = require('node-fetch');
 const request = require('request');
 
+//Original Request Using request and a callback function
+
 // request.get('http://httpbin.org/get', (error, response, body) => {
 // 	if (error) {
 // 		console.error('Oh shoot. Something went wrong:');
@@ -13,11 +15,29 @@ const request = require('request');
 // 	console.log('Request done. Response status code: %d', response.statusCode);
 // });
 
+//Refactored to use Fetch which returns a promise
+
+// fetch('http://httpbin.org/get')
+// .then(response => {
+//   console.log('Request using Promises done. Response status code: %d', response.status);
+// })
+// .catch(error => {
+//   console.error('Oh shoot. Something went wrong with the promise code:');
+//   console.error(error.message);
+// });
+
+//Refactored to use a second .then()
+
+function extractStatusCode(response) {
+  return response.status;
+}
+
 fetch('http://httpbin.org/get')
-.then(response => {
-  console.log('Request using Promises done. Response status code: %d', response.status);
+.then(extractStatusCode)
+.then(statusCode => {
+  console.log('Request using Promises, part II. Response status code: %s', statusCode);
 })
 .catch(error => {
-  console.error('Oh shoot. Something went wrong with the promise code:');
-  console.error(error.message);
+  console.error('Oh shoot. Something went wrong with the promise code:');
+  console.error(error.message);
 });
