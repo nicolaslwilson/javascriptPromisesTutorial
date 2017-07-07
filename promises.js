acaac87b0ee64b340a7be118ca22c9cc59b956af
Nic Wilson <nicwilson@gmail.com>
2017-07-07 12:57:51 -0500

    Fetch using a Promise

--- promises.js
+++ promises.js
@@ -3,12 +3,21 @@
 const fetch = require('node-fetch');
 const request = require('request');
 
-request.get('http://httpbin.org/get', (error, response, body) => {
-	if (error) {
-		console.error('Oh shoot. Something went wrong:');
-		console.error(error.message);
-		return;
-	}
+// request.get('http://httpbin.org/get', (error, response, body) => {
+// 	if (error) {
+// 		console.error('Oh shoot. Something went wrong:');
+// 		console.error(error.message);
+// 		return;
+// 	}
 
-	console.log('Request done. Response status code: %d', response.statusCode);
+// 	console.log('Request done. Response status code: %d', response.statusCode);
+// });
+
+fetch('http://httpbin.org/get')
+.then(response => {
+  console.log('Request using Promises done. Response status code: %d', response.status);
+})
+.catch(error => {
+  console.error('Oh shoot. Something went wrong with the promise code:');
+  console.error(error.message);
 });
