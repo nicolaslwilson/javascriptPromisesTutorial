7fdb32c80eb8565a5131a5a8c6d70610afaafaac
Nic Wilson <nicwilson@gmail.com>
2017-07-07 12:53:10 -0500

    Request using callbacks

--- promises.js
+++ promises.js
@@ -3,3 +3,12 @@
 const fetch = require('node-fetch');
 const request = require('request');
 
+request.get('http://httpbin.org/get', (error, response, body) => {
+	if (error) {
+		console.error('Oh shoot. Something went wrong:');
+		console.error(error.message);
+		return;
+	}
+
+	console.log('Request done. Response status code: %d', response.statusCode);
+});
